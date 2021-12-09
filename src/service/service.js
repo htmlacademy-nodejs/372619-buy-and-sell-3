95865835d259fe2a86a34b6d7843c9d083e9391e
Ryabov Maxim <ryabovms@altarix.ru>
2021-12-09 21:08:29 +0400

    module1-task3 console generator was created

--- src/service/service.js
+++ src/service/service.js
@@ -1,3 +1,18 @@
-"use strict";
+'use strict';
 
-console.log(`Hello World!`);
+const {Cli} = require(`./cli`);
+const {
+  DEFAULT_COMMAND,
+  USER_ARGV_INDEX,
+  ExitCode
+} = require(`../constants`);
+
+const userArgs = process.argv.slice(USER_ARGV_INDEX);
+const [userCommand] = userArgs;
+
+if (!userArgs.length || !Cli[userCommand]) {
+  Cli[DEFAULT_COMMAND].run();
+  process.exit(ExitCode.success);
+}
+
+Cli[userCommand].run(userArgs.slice(1));
